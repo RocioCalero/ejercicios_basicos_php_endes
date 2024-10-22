aae4ed660283393b96010d750501e5804b3695bc
rocio <162433529+RocioCalero@users.noreply.github.com>
2024-10-22 12:42:56 +0200

    index.php

---
+++ ejercicio_5/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dia de la semana</title>
+</head>
+<body>
+    <h1>Dia de la semana</h1>
+    <form method="post" action="dia_Semana.php">
+        <label for="num">Introduzca n:</label>
+        <input type="number" id="num" name="num" step="0.01" required>
+        <input type="submit" value="Enviar">
+</body>
+</html>
